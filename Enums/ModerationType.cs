61265799c6a5557c86d462d2042bf2e1713e9bd9
justarunsthing <justarunsthing@outlook.com>
2025-04-12 14:27:43 +0100

    Added ReadyStatus & ModerationType enums

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Blog.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political Propaganda")]
+        Political,
+
+        [Description("Offensive Language")]
+        Language,
+
+        [Description("Drug References")]
+        Drugs,
+
+        [Description("Threatening Speech")]
+        Threats,
+
+        [Description("Sexual content")]
+        Sexual,
+
+        [Description("Hate Speech")]
+        HateSpeech,
+
+        [Description("Targeted Shaming")]
+        Shaming
+    }
+}
