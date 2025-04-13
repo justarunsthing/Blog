ae924602dccbb0e78ee313f62e5654b8f4378a0b
justarunsthing <justarunsthing@outlook.com>
2025-04-13 20:18:11 +0100

    Updated scaffolded code

--- Models/Post.cs
+++ Models/Post.cs
@@ -7,6 +7,8 @@ namespace Blog.Models
     public class Post
     {
         public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
         public int BlogId { get; set; }
         public string AuthorId { get; set; }
 
