fc6e6e2530ba6f7185b16962536e0d57c1291666
justarunsthing <justarunsthing@outlook.com>
2025-04-19 11:58:31 +0100

    Implemented slug service

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -7,16 +7,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Blog.Data;
 using Blog.Models;
+using Blog.Interfaces;
 
 namespace Blog.Controllers
 {
     public class PostsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ISlugService _slugService;
 
-        public PostsController(ApplicationDbContext context)
+        public PostsController(ApplicationDbContext context, ISlugService slugService)
         {
             _context = context;
+            _slugService = slugService;
         }
 
         // GET: Posts
@@ -62,13 +65,25 @@ namespace Blog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BlogId,Title,Abstract,Content,Status,Image")] Post post)
+        public async Task<IActionResult> Create([Bind("BlogId,Title,Abstract,Content,Status,Image")] Post post, List<string> tagValues)
         {
             if (ModelState.IsValid)
             {
                 post.Created = DateTime.UtcNow;
 
+                var slug = _slugService.UrlFriendly(post.Title);
+
+                if (!_slugService.IsUnique(slug))
+                {
+                    ModelState.AddModelError("Title", "The title you provided cannot be used for unique slug");
+                    ViewData["TagValues"] = string.Join(",", tagValues);
+
+                    return View(post);
+                }
+
+                post.Slug = slug;
                 _context.Add(post);
+
                 await _context.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
